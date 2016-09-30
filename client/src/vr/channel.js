87d6b8cbc9a0c1666289e4305b50ccb65b3dfebb
tincho <tinchoz49@gmail.com>
2016-09-30 18:54:43 -0300

    remove aframe particle system

--- client/src/vr/channel.js
+++ client/src/vr/channel.js
@@ -1,3 +1,5 @@
+require('./explosion');
+
 const colors = [
     '#e00b0b',
     '#477bff',
@@ -16,21 +18,6 @@ function getColor(data) {
     return color;
 }
 
-function updateEmitterColor(emitter, data) {
-    const color = getColor(data);
-    for (let i = 0; i < 4; i++) {
-        emitter.color.value[i].setStyle(color);
-    }
-    emitter.color.value = emitter.color.value;
-}
-
-function updateSettingsColor(particleGroup, data) {
-    const color = getColor(data);
-    particleGroup._poolCreationSettings.color.value = [new THREE.Color(color)];
-}
-
-const pos = new THREE.Vector3();
-
 module.exports = function (rootOSCServer) {
 
     AFRAME.registerComponent('channel', {
@@ -44,51 +31,25 @@ module.exports = function (rootOSCServer) {
             const id = this.data.id;
             const el = this.el;
             const sphere = el.querySelector('.sphere');
-            const particleGroup = el.components['particle-system'].particleGroup;
-            particleGroup.removeEmitter(particleGroup.emitters[0]);
-
-            particleGroup.addPool(10, {
-                type: 2,
-                position: {
-                    spread: new THREE.Vector3(3),
-                    radius: 1
-                },
-                velocity: {
-                    value: new THREE.Vector3(10)
-                },
-                size: {
-                    value: [5, 0]
-                },
-                opacity: {
-                    value: [1, 0]
-                },
-                color: {
-                    value: [new THREE.Color('white')]
-                },
-                particleCount: 100,
-                duration: 0.10,
-                maxAge: {
-                    value: 1
-                }
-            }, false);
+            const explosion = el.components.explosion;
 
             rootOSCServer.on(`raw-data:channel-${id}`, (data) => {
-                if (particleGroup._pool.length) {
-                    updateEmitterColor(particleGroup._pool[particleGroup._pool.length - 1], data);
-                } else {
-                    updateSettingsColor(particleGroup, data);
-                }
-                particleGroup.triggerPoolEmitter(1, pos.setFromMatrixPosition(sphere.object3D.matrixWorld));
+                explosion.create({
+                    color: getColor(data),
+                    matrixWorld: sphere.object3D.matrixWorld
+                });
             });
         }
     });
 
     return function createChannel(parent, id) {
         parent.insertAdjacentHTML('beforeend', `
-        <a-entity id="channel-${id}" particle-system="texture: images/flare-2.png;" channel="id: ${id};">
-            <a-sphere class="sphere" wireframe radius="0.5" position="18 0 ${id}" color="#bc2f2f" metalness="0.5">
+        <a-entity id="channel-${id}" explosion channel="id: ${id};">
+            <a-sphere class="sphere" wireframe radius="0.5" position="${id} 0 -20" color="#bc2f2f" metalness="0.5">
+                <a-animation attribute="rotation" to="-360 -360 0" dur="5000" easing="linear" repeat="indefinite">
+                </a-animation>
             </a-sphere>
-            <a-animation attribute="rotation" to="0 -360 0" dur="5000" easing="linear" repeat="indefinite">
+            <a-animation attribute="rotation" to="0 -360 0" dur="10000" easing="linear" repeat="indefinite">
             </a-animation>
         </a-entity>
         `);
---
+++ client/src/vr/explosion.js
@@ -0,0 +1,75 @@
+const SPE = require('shader-particle-engine');
+const raf = require('raf');
+const pos = new THREE.Vector3();
+
+function updateColor(particleGroup, color) {
+    if (particleGroup._pool.length) {
+        const emitter = particleGroup._pool[particleGroup._pool.length - 1];
+        for (let i = 0; i < 4; i++) {
+            emitter.color.value[i].setStyle(color);
+        }
+        emitter.color.value = emitter.color.value;
+    }
+    particleGroup._poolCreationSettings.color.value = [new THREE.Color(color)];
+}
+
+function trigger(options) {
+    if (this.lastColor !== options.color) {
+        updateColor(this.particleGroup, options.color);
+        this.lastColor = options.color;
+    }
+    this.particleGroup.triggerPoolEmitter( 1, pos.setFromMatrixPosition(options.matrixWorld) );
+}
+
+AFRAME.registerComponent('explosion', {
+    schema: {},
+    init() {
+        this.emitterSettings = {
+            type: SPE.distributions.SPHERE,
+            position: {
+                spread: new THREE.Vector3(3),
+                radius: 1
+            },
+            velocity: {
+                value: new THREE.Vector3( 10 )
+            },
+            size: {
+                value: [ 30, 0 ]
+            },
+            opacity: {
+                value: [1, 0]
+            },
+            color: {
+                value: [new THREE.Color('yellow')]
+            },
+            particleCount: 100,
+            alive: true,
+            duration: 0.05,
+            maxAge: {
+                value: 0.5
+            }
+        };
+    },
+    update() {
+        this.clock = new THREE.Clock();
+        this.lastColor = 'yellow';
+
+        this.particleGroup = new SPE.Group({
+            texture: {
+                value: THREE.ImageUtils.loadTexture('./images/flare-2.png')
+            },
+            blending: THREE.AdditiveBlending
+        });
+
+        this.particleGroup.addPool( 10, this.emitterSettings, false );
+
+        this.el.sceneEl.object3D.add(this.particleGroup.mesh);
+
+        this.create = (options) => {
+            raf(() => trigger.call(this, options));
+        };
+    },
+    tick() {
+        this.particleGroup.tick(this.clock.getDelta());
+    }
+});
